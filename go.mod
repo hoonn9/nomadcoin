d4734c3f76116c76658dd07d35249e41b62eccff
hoonn9 <hun9935@gmail.com>
2021-07-08 23:04:26 +0900

    3.0 Creating the Project, 3.1 Variables in Go

---
+++ go.mod
@@ -0,0 +1,4 @@
+module github.com/hoonn9/nomadcoin
+// 모듈의 보관 장소
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const constant string = "cool"
+
+func main() {
+	// var name string = "hello"
+	name := "hello"
+	// shortcut 변수 선언을 함수 밖에서 사용 불가
+	name = "hoon"
+	fmt.Println(name)
+}
+
+var test string = "dasd"
