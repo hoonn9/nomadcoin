d8ee993da2303a4f3c28cca0ba40950b12287e82
hoonn9 <hun9935@gmail.com>
2021-07-10 22:51:13 +0900

    3.3 fmt

--- main.go
+++ main.go
@@ -2,27 +2,17 @@ package main
 
 import "fmt"
 
-// func plus (a, b int, name string) (int, string) {
-// 	return a + b, name
-// }
-
-// func plus (a ...int) (int) {
-// 	total := 0
-// 	for _, item := range a {
-// 		total += item
-// 	}
-// 	return total
-// }
-
-
 
 func main() {
-	// result := plus(2, 3, 4, 5, 6, 7)
-	name := "hoon ! ! ! ! ! ! Is my name"
+	x := 31254125234
+	// binary 2
+	// sprintf => format 된 데이터 반환
+	xAsBinary := fmt.Sprintf("%b\n", x)
+	fmt.Println(x, xAsBinary)
+	// // 8
+	// fmt.Printf("%o\n", x)
+	// // 16
+	// fmt.Printf("%x\n", x)
 
-	// 기본적으로 byte로 넘겨줌
-	for _, letter := range name {
-		fmt.Println(string(letter))
-		fmt.Printf("%x", letter)
-	}
+
 }
