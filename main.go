26940278e8a310a5aa462b7a95f2a550bca52148
hoonn9 <hun9935@gmail.com>
2021-07-11 18:22:52 +0900

    4.2 Our First Blockchain

--- main.go
+++ main.go
@@ -25,22 +25,38 @@ type block struct {
 		b3Hash = (data + b2Hash)
 */
 
+type blockchain struct {
+	blocks []block
+}
+
+func (b *blockchain) getLastHash() string {
+	if len(b.blocks) > 0 {
+		return b.blocks[len(b.blocks) - 1].hash
+	}
+	return ""
+}
+
+func (b *blockchain) addBlock(data string) {
+	newBlock := block{data, "", b.getLastHash()}
+	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
+	newBlock.hash = fmt.Sprintf("%x", hash)
+	b.blocks = append(b.blocks, newBlock)
+}
+
+func (b *blockchain) listBlocks() {
+	for _, block := range b.blocks {
+		fmt.Printf("Data: %s\n", block.data)
+		fmt.Printf("Hash: %s\n", block.hash)
+		fmt.Printf("Prev Hash: %s\n", block.prevHash)
+	}
+}
+
 func main() {
-	// for _, aByte := range "Genesis Block" {
-	// 	fmt.Printf("%b\n",aByte)
-	// }
+	chain := blockchain{}
+	chain.addBlock("Genesis Block")
+	chain.addBlock("Second Block")
 
-	genesisBlock := block{"Genesis Block", "", ""}
-	fmt.Println(genesisBlock)
-	// slice of byte
-
-	/*
-		SHA256
-	*/
-
-	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.hash))
-	// 16진수로 format base16 => string으로 변환
-	hexHash := fmt.Sprintf("%x",hash)
-	genesisBlock.hash = hexHash
-	fmt.Println(genesisBlock)
+	chain.addBlock("Third Block")
+	chain.listBlocks()
+	
 }
