506ac63ca63935484928ee740336720f313ae1d0
hoonn9 <hun9935@gmail.com>
2021-07-11 18:04:08 +0900

    4.1 Our First Block

--- main.go
+++ main.go
@@ -1,17 +1,46 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
-
-	"github.com/hoonn9/nomadcoin/person"
 )
 
+type block struct {
+	data string
+	hash string
+	prevHash string
+}
 
+// 단방향 함수
+// 입력 값으로 얻은 출력값을 출력값을 통해 입력값을 얻을 수 없음
+
+/*
+	하나의 hash만 변해도 전체가 변함. 무효화 된다.
+
+	B1
+		b1Hash = (data + "")
+	B2
+		b2Hash = (data + b1Hash)
+	B3
+		b3Hash = (data + b2Hash)
+*/
 
 func main() {
-	// export는 Uppercase 시작으로 설정
-	// struct field도 export 하면 직접 접근이 불가능하다.
-	hoon := person.Person{}
-	hoon.SetDetails("hoon", 26)
-	fmt.Println("Main 'hoon'", hoon)
+	// for _, aByte := range "Genesis Block" {
+	// 	fmt.Printf("%b\n",aByte)
+	// }
+
+	genesisBlock := block{"Genesis Block", "", ""}
+	fmt.Println(genesisBlock)
+	// slice of byte
+
+	/*
+		SHA256
+	*/
+
+	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.hash))
+	// 16진수로 format base16 => string으로 변환
+	hexHash := fmt.Sprintf("%x",hash)
+	genesisBlock.hash = hexHash
+	fmt.Println(genesisBlock)
 }
