c6ef921ff819d8021bdfca84ec60a83f443b3671
hoonn9 <hun9935@gmail.com>
2021-07-23 16:40:33 +0900

    6 Rest Api 6.0 Setup

--- main.go
+++ main.go
@@ -1,9 +1,46 @@
 package main
 
-import "github.com/hoonn9/nomadcoin/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/hoonn9/nomadcoin/utils"
+)
+
+const port string = ":4000"
 
 
+type URLDescription struct {
+	URL 		string
+	Method 		string
+	Description string
+}
+/**
+	Marshal 
+	메모리형식으로 저장된 객체를 저장, 송신 가능하게 만들어 주는 것
+	Unmarshal
+	반대
+*/
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL: "/",
+			Method: "GET",
+			Description: "See Documentation",
+		},
+	}
+	// slice to json(byte[])
+	b, err := json.Marshal(data)
+	utils.HandleErr(err)
+	fmt.Println(b)
+	fmt.Printf("%s", b)
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+
+	fmt.Printf("Listening on http://localhost%s",port)
+	log.Fatal(http.ListenAndServe(port,  nil))
 }
