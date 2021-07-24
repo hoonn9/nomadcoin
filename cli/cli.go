57905bc68eda71a5cedcdb77808690e634412b20
hoonn9 <hun9935@gmail.com>
2021-07-24 23:21:28 +0900

    7.3 Flag

---
+++ cli/cli.go
@@ -0,0 +1,66 @@
+package cli
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/hoonn9/nomadcoin/explorer"
+	"github.com/hoonn9/nomadcoin/rest"
+	"github.com/hoonn9/nomadcoin/utils"
+)
+
+func usage() {
+	fmt.Printf("Welcome to Nomad Coin\n")
+	fmt.Printf("Please use the following flags:\n\n")
+	fmt.Printf("-port:		Set the PORT of the server\n")
+	fmt.Printf("-mode:		Choose between 'html' and 'rest'\n")
+	// error code 0 => 에러 없음
+	os.Exit(0)
+}
+
+// CLI (Command Line Interface)
+// only using flag package (cobra's famous cli framework but not using)
+func Start() {
+	if len(os.Args) == 1 {
+		usage()
+	}
+
+	port := flag.String("port", "4000", "Set port of th server. if mode is all, input two port ex) 3000:4000")
+	mode := flag.String("mode", "rest", "Choose between 'html' and 'rest' and 'all'")
+
+	flag.Parse()
+
+	switch *mode {
+	case "rest":
+		port, err := strconv.Atoi(*port)
+		utils.HandleErr(err)
+		rest.Start(port)
+	case "html":
+		port, err := strconv.Atoi(*port)
+		utils.HandleErr(err)
+		explorer.Start(port)
+	case "all":
+		slices := strings.Split(*port,":")
+
+		if len(slices) < 2 {
+			log.Fatal(errors.New("all mode required two port. <rest port>:<html port> (ex: 4000:5000)"))
+		}
+
+		for index, item := range(slices) {
+			port, err := strconv.Atoi(item)
+			utils.HandleErr(err)
+			if index == 0 {
+				go rest.Start(port)
+			} else {
+				explorer.Start(port)
+			}
+		}
+	default:
+		usage()
+	}
+}
--- main.go
+++ main.go
@@ -1,49 +1,8 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-)
+import "github.com/hoonn9/nomadcoin/cli"
 
-func usage() {
-	fmt.Printf("Welcome to Nomad Coin\n")
-	fmt.Printf("Please use the following commands:\n\n")
-	fmt.Printf("explorer:	Start the HTML Explorer\n")
-	fmt.Printf("rest:	Start the REST API(recommanded)\n")
-	// error code 0 => 에러 없음
-	os.Exit(0)
-}
 
-// CLI (Command Line Interface)
-// only using flag package (cobra's famous cli framework but not using)
-// command  ex) start
-// flag  ex) -port=3000  -https
 func main() {
-	// fmt.Println(os.Args[2:])
-	if len(os.Args) < 2 {
-		usage()
-	}
-	// flag set  => command 에 따른 flag 들을 set
-	rest := flag.NewFlagSet("rest", flag.ExitOnError)
-
-	// 타입 설정, flag 명, default 값, 선택 시 띄워줄 문구
-	portFlag := rest.Int("port", 4000, "Sets the port of the server")
-
-	// command
-	switch os.Args[1] {
-	case "explorer":
-		fmt.Println("Start Explorer")
-	case "rest":
-		rest.Parse(os.Args[2:])
-		fmt.Println("Start REST API")
-	default:
-		usage()
-	}
-
-	// rest가 parse 됐을 때
-	if rest.Parsed() {
-		fmt.Println(portFlag)
-		fmt.Println("Start server")
-	}
+	cli.Start()
 }
