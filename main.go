931992acfa4e6638db3957744f2832e3e9c54900
hoonn9 <hun9935@gmail.com>
2021-09-12 00:51:27 +0900

    12.2 Channels

--- main.go
+++ main.go
@@ -5,16 +5,26 @@ import (
 	"time"
 )
 
-func countToTen(name string)  {
+func countToTen(c chan int)  {
+	// 10번 blocking
 	for i := range [10]int{} {
-		fmt.Println(i, name)
-	 	time.Sleep(1 * time.Second)
+		time.Sleep(1 * time.Second)
+		fmt.Printf("sending %d", i)
+		c <- i
  	}
 }
 
 func main() {
-	go countToTen("first")
-	go countToTen("second")
+	c := make(chan int)
 
-	for {}
+	go countToTen(c)
+
+	// blocking operation : 프로그램을 기다림 (a := <- c, for {})
+
+	for {
+		a := <- c
+		fmt.Printf("received %d\n", a)
+	}
+
+	// 끝난 go routine의 channel message를 계속 기다리면 deadlock
 }
