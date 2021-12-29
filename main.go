fc381c63212260985e7ff93ac88bd16ac8bc5cdc
hoonn9 <hun9935@gmail.com>
2021-12-29 16:49:42 +0900

    12.4 Buffered Channels

--- main.go
+++ main.go
@@ -6,12 +6,13 @@ import (
 )
 
 // chan<- => only send channel
-func countToTen(c chan<- int)  {
+func send(c chan<- int)  {
 	// 10번 blocking
 	for i := range [10]int{} {
-		time.Sleep(1 * time.Second)
-		fmt.Printf("sending %d", i)
+		fmt.Printf(">> sending %d << \n", i)
+		// sender blocking (실행된 함수에서 receiving 중 그 함수도 blocking 된다.)
 		c <- i
+		fmt.Printf("sent %d \n", i)
 		
  	}
 	 // go routine close
@@ -22,6 +23,7 @@ func countToTen(c chan<- int)  {
 func receive(c <-chan int) {
 
 	for {
+		time.Sleep(10 * time.Second)
 		// ok: boolean => channel open 여부
 		a, ok := <- c
 
@@ -29,7 +31,7 @@ func receive(c <-chan int) {
 			fmt.Println("done.")
 			break
 		}
-		fmt.Printf("received %d\n", a)
+		fmt.Printf("|| received %d ||\n", a)
 	}
 }
 
@@ -38,8 +40,13 @@ func main() {
 	// blocking operation : 프로그램을 기다림 (a := <- c, for {})
 	// 끝난 go routine의 channel message를 계속 기다리면 deadlock
 
-	c := make(chan int)
+	// unbuffer channel은 큐 공간이 1개인 기본 상태 (all blocking)
 
-	go countToTen(c)
+	// buffer channel (n 개당 block처리)
+	// 큐에 보내기를 n개 채우고, 받기 할떄마다 하나씩 제거됨
+	// [1,2,3,4,5] 한번에 보내기 => 10초 기다린 후 1받고 6보내기 큐에 추가...
+	c := make(chan int, 10)
+
+	go send(c)
 	receive(c)
 }
