754c98000a0ea12e1a08698bd13958f807c4727f
hoonn9 <hun9935@gmail.com>
2021-12-30 19:08:35 +0900

    12.12 Recap

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,7 +3,6 @@ package p2p
 import (
 	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/gorilla/websocket"
 	"github.com/hoonn9/nomadcoin/utils"
@@ -12,21 +11,22 @@ import (
 var upgrader = websocket.Upgrader{}
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
+
+	openPort := r.URL.Query().Get("openPort")
+	ip := utils.Splitter(r.RemoteAddr, ":", 0)
+
 	upgrader.CheckOrigin = func(r *http.Request) bool {
-		return true
+		return openPort != "" && ip != ""
 	}
+
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
-	openPort := r.URL.Query().Get("openPort")
-
-	// remoteAddr 에는 실제 요청한 서버의 Open port가 업다.
-	result := strings.Split(r.RemoteAddr, ":")
-	initPeer(conn, result[0], openPort)
+	initPeer(conn, ip, openPort)
 
 }
 
 func AddToPeer(address, port, openPort string)  {
-	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort), nil)
+	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
 
 	initPeer(conn, address, port)
