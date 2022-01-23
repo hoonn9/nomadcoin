1abcd1f88e0df7ccd391e986865f7a02e2f2ce72
hoonn9 <hun9935@gmail.com>
2022-01-23 20:06:47 +0900

    12.30 Global Memory Pool

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -16,7 +16,7 @@ const (
 )
 
 type mempool struct {
-	Txs []*Tx
+	Txs map[string]*Tx
 	m		sync.Mutex
 }
 
@@ -26,7 +26,9 @@ var memOnce sync.Once
 
 func Mempool() *mempool {
 	memOnce.Do(func() {
-		m = &mempool{}
+		m = &mempool{
+			Txs: make(map[string]*Tx),
+		}
 	})
 	return m
 }
@@ -188,17 +190,21 @@ func (m *mempool) AddTx(to string, amount int) (*Tx,error) {
 		return nil, err
 	}
 
-	m.Txs = append(m.Txs, tx)
+	m.Txs[tx.ID] =  tx
 	return tx, nil
 }
 
 func (m *mempool) txToConfirm() []*Tx {
 	coinbase := makeCoinbaseTx(wallet.Wallet().Address)
 
-	// tx 개수의 제한을 두지 않음
-	txs := m.Txs
+	var txs []*Tx
+
+	for _, tx := range m.Txs {
+		txs = append(txs, tx)
+	}
+
 	txs = append(txs, coinbase)
-	m.Txs = nil
+	m.Txs = make(map[string]*Tx)
 
 	return txs
 }
@@ -207,5 +213,5 @@ func (m *mempool) AddPeerTx(tx *Tx) {
 	m.m.Lock()
 	defer m.m.Unlock()
 
-	m.Txs = append(m.Txs, tx)
+	m.Txs[tx.ID] = tx
 }
