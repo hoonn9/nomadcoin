53b4f6bc026dade1d847902de65f4e8398a08ca6
hoonn9 <hun9935@gmail.com>
2022-01-24 22:59:55 +0900

    13.4 Table Tests 13.3 Coverage

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -2,7 +2,9 @@ package utils
 
 import (
 	"encoding/hex"
+	"errors"
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -36,4 +38,52 @@ func ExampleHash() {
 	fmt.Println(x)
 	// Output: 1cb6fa80c3cf0c4449500e4358cd5cdab5cbc43e035563c65ba4513d0c3fd116
 
+}
+
+func TestToBytes(t *testing.T) {
+	s := "test"
+	b := ToBytes(s)
+	kind := reflect.TypeOf(b).Kind() 
+	if kind != reflect.Slice {
+		t.Errorf("ToBytes should return a slice of bytes got %s", kind)
+	}
+}
+
+func TestSplitter(t *testing.T) {
+	type test struct {
+		input string
+		sep 	string
+		index	int
+		output string
+	}
+
+	tests := []test{
+		{input: "0:6:0", sep: ":", index: 1, output: "6"},
+		{input: "0:6:0", sep: ":", index: 10, output: ""},
+		{input: "0:6:0", sep: "/", index: 0, output: "0:6:0"},
+	}
+
+	for _, tc := range tests {
+		got := Splitter(tc.input, tc.sep, tc.index)
+		if got != tc.output  {
+			t.Errorf("Expected %s and got %s", tc.output, got)
+		}
+	}
+}
+
+func TestHandleErr(t *testing.T) {
+	oldLogFn := logFn
+	defer func() {
+		logFn = oldLogFn
+	}()
+	called := false
+	logFn = func(v ...interface{}) {
+		called = true
+	}
+	err := errors.New("test")
+	HandleErr(err) 
+
+	if !called {
+		t.Error("HandleErr should call fn")
+	}
 }
