65ca41f0bc115850272ff877770c6b9a59472124
hoonn9 <hun9935@gmail.com>
2022-01-24 23:08:24 +0900

    13.5 Full Coverage

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"reflect"
@@ -85,5 +86,39 @@ func TestHandleErr(t *testing.T) {
 
 	if !called {
 		t.Error("HandleErr should call fn")
+	}
+}
+
+func TestFromBytes(t *testing.T) {
+	type testStruct struct {
+		Test string
+	}
+	var restored testStruct
+	ts := testStruct{"test"}
+	b := ToBytes(ts)
+
+	FromBytes(&restored, b)
+
+	if !reflect.DeepEqual(ts, restored) {
+		t.Error("FromBytes should restore struct")
+	}
+}
+
+func TestToJSON(t *testing.T) {
+	type testStruct struct{Test string}
+	s := testStruct{"test"}
+
+	b := ToJSON(s)
+	kind := reflect.TypeOf(b).Kind()
+	
+	if kind != reflect.Slice {
+		t.Errorf("Expected %v and got %v", reflect.Slice, kind)
+	}
+
+	var restored testStruct
+	json.Unmarshal(b, &restored)
+
+	if !reflect.DeepEqual(s, restored) {
+		t.Error("ToJSON should encode to JSON correctly")
 	}
 }
